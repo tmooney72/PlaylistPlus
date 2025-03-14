a841002bd6b17f5d84e1e70b87a77104dd681bb9
tylermooney72 <tylermooney32@gmail.com>
2025-03-13 21:27:16 -0500

    removed again

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import { meta } from "@eslint/js";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,14 +7,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAZIIMMKrMBKo1alk0kJMbMyStZ8wYdQzA",
-  authDomain: "playlistplus-66399.firebaseapp.com",
-  projectId: "playlistplus-66399",
-  storageBucket: "playlistplus-66399.firebasestorage.app",
-  messagingSenderId: "633654046546",
-  appId: "1:633654046546:web:e1a2a109c1b835ccc8948b",
-  measurementId: "G-KWJ2V58NGX"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
 };
 
 // Initialize Firebase
