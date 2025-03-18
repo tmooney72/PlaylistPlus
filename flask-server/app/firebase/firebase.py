7a6a6af83e2cf659248b06b2e50e1ff520cd46fc
tylermooney72 <tylermooney32@gmail.com>
2025-03-18 10:45:27 -0700

    please

--- flask-server/app/firebase/firebase.py
+++ flask-server/app/firebase/firebase.py
@@ -1,7 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+from dotenv import load_dotenv
+import json
 
 # Update the path to your service account key JSON file
-cred = credentials.Certificate("/Users/tylermooney/Desktop/PlaylistPlus/flask-server/app/firebase/firebaseinfo.json")
+load_dotenv()
+firebase_info = os.environ.get('FIREBASE_INFO')
+if not firebase_info:
+    raise ValueError("FIREBASE_INFO environment variable is not set.")
+firebase_info_dict = json.loads(firebase_info)
+cred = credentials.Certificate(firebase_info_dict)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
