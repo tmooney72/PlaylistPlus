cf19356e7dd1eb90226e9058edfbbf041d6382cd
tylermooney72 <tylermooney32@gmail.com>
2025-03-19 20:36:24 -0700

    small

--- flask-server/app/home.py
+++ flask-server/app/home.py
@@ -5,7 +5,7 @@ from flask import redirect
 def home():
     # Check if there's a valid cached token, if not, redirect to Spotify login
     token_info = cache_handler.get_cached_token()
-    if not token_info and not sp_oauth.validate_token(token_info):
+    if not token_info or not sp_oauth.validate_token(token_info):
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
     return redirect("http://localhost:5173/Home")
