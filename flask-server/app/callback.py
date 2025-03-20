7592a6656b2e31684d9556bd3c8d5ae6f01c58e0
tylermooney72 <tylermooney32@gmail.com>
2025-03-19 20:18:02 -0700

    hopefully fixed redis error

--- flask-server/app/callback.py
+++ flask-server/app/callback.py
@@ -9,16 +9,12 @@ def callback():
         if not code:
             return "Authorization code missing", 400
 
-        # Retrieve cached token if it exists
-        token_info = cache_handler.get_cached_token()
+        # Get new token with the authorization code
+        token_info = sp_oauth.get_access_token(code)
 
-        # If no cached token, request a new token
-        if not token_info:
-            token_info = sp_oauth.get_access_token(code)
-
-            # If the token is a string, wrap it in a dictionary
-            if isinstance(token_info, str):
-                token_info = {"access_token": token_info}
+        # If the token is a string, wrap it in a dictionary
+        if isinstance(token_info, str):
+            token_info = {"access_token": token_info}
 
         # The token is automatically stored in Flask's session via the cache handler
         return redirect("http://localhost:5173/Home")
