ed09e2316e2954fae3b3f872674cbe61e98ec62b
tylermooney72 <tylermooney32@gmail.com>
2025-03-24 14:01:49 -0500

    trying no session and no after request

--- flask-server/app/__init__.py
+++ flask-server/app/__init__.py
@@ -11,16 +11,6 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 app = Flask(__name__)
 app.config['SCHEDULER_API_ENABLED'] = True
 app.config['SECRET_KEY'] = os.urandom(64)
-app.config['SESSION_TYPE'] = 'redis'
-redis_url = os.getenv('REDIS_URL')
-if not redis_url:
-    raise ValueError("REDIS_URL environment variable is not set")
-app.config['SESSION_REDIS'] = redis.from_url(redis_url)
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
-app.config['SESSION_COOKIE_SECURE'] = True  # Changed to False for local development
-app.config['SESSION_COOKIE_HTTPONLY'] = True
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-Session(app)  # Initialize the session interface
 
 print('this is running') #this works
 
@@ -31,14 +21,14 @@ CORS(app,
      )
 
 # Add a decorator to handle preflight requests
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'https://playlist-plus.vercel.app')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,Cookie')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    response.headers.add('Access-Control-Expose-Headers', 'Set-Cookie')
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', 'https://playlist-plus.vercel.app')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,Cookie')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#     response.headers.add('Access-Control-Allow-Credentials', 'true')
+#     response.headers.add('Access-Control-Expose-Headers', 'Set-Cookie')
+#     return response
 
 # Spotify configuration (shared across modules)
 client_id = 'e38944e89ce74ba691862c01183972ed'
@@ -51,7 +41,6 @@ from spotipy.cache_handler import FlaskSessionCacheHandler
 from spotipy import Spotify
 
 # Create the cache handler and Spotify objects
-cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
